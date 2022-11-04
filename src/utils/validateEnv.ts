140678f6295f47dd7a85d0c301efc1e8619514f1
darkmift <darkmift@gmail.com>
2022-11-04 10:12:30 +0200

    added failure log,more validators

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,10 +1,25 @@
-import { cleanEnv, port, str } from 'envalid';
+import { cleanEnv, port, str, host } from 'envalid';
+import { logger } from './logger';
+
+const validators = {
+    NODE_ENV: str(),
+    PORT: port(),
+    DB_HOST: host(),
+    DB_PORT: port(),
+    DB_USER: str(),
+    DB_PASSWORD: str(),
+    DB_DATABASE: str(),
+    SECRET_KEY: str(),
+};
+
+const options = {
+    reporter: ({ errors, env }: any) => {
+        logger.info('Invalid env vars: ' + env + Object.keys(errors));
+    },
+};
 
 function validateEnv() {
-    cleanEnv(process.env, {
-        NODE_ENV: str(),
-        PORT: port(),
-    });
+    cleanEnv(process.env, validators, options);
 }
 
 export default validateEnv;
