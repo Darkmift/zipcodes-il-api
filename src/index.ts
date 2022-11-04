4a94dc7820e51999762456181c5da653e740b0b5
Darkmift <darkmift@gmail.com>
2022-11-04 09:09:52 +0200

    Initialize sequelize-boilerplate

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import { NODE_ENV, PORT, LOG_FORMAT } from '@config';
+import validateEnv from './utils/validateEnv';
+import { logger, stream } from '@utils/logger';
+import DB from './databases';
+
+validateEnv();
+
+DB.sequelize.sync({ force: false });
+
+dotenv.config();
+const app: Express = express();
+const port = PORT || 5000;
+
+app.use(morgan(LOG_FORMAT, { stream }));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[NODE_ENV]: ${NODE_ENV}`);
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+
+    logger.info(`=================================`);
+    logger.info(`======= ENV: ${NODE_ENV} =======`);
+    logger.info(`🚀 [server]: Server is running at https://localhost:${port}`);
+    logger.info(`=================================`);
+});
