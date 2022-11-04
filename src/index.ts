4befa60d2e0d33a185623894b367773e363ca5fb
darkmift <darkmift@gmail.com>
2022-11-04 11:10:15 +0200

    DB connection health logic check,shutdown log

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,55 @@ import dotenv from 'dotenv';
 import { NODE_ENV, PORT, LOG_FORMAT } from '@config';
 import validateEnv from './utils/validateEnv';
 import { logger, stream } from '@utils/logger';
-
-validateEnv();
+import DB from './database';
 
 dotenv.config();
+validateEnv();
+
+try {
+    DB.sequelize.sync({ force: false });
+} catch (error) {
+    const errorMessage = 'DB sync failed';
+    logger.error(errorMessage);
+
+    if (error instanceof Error) {
+        error.message = errorMessage + '\n' + error.message;
+    }
+    throw error;
+}
+
 const app: Express = express();
 const port = PORT || 5000;
 
 app.use(morgan(LOG_FORMAT, { stream }));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.get('/health', async (req: Request, res: Response) => {
+    const data: any = {
+        uptime: process.uptime(),
+        message: 'Ok',
+        date: new Date(),
+        sqlConnection: null,
+    };
+    try {
+        const [results] = await DB.sequelize.query(
+            `SHOW TABLES LIKE "location";`,
+        );
+
+        if (!results[0]) {
+            throw new Error('Error Query Health SQL');
+        }
+        data.sqlConnection = 'Ok';
+    } catch (error) {
+        if (error instanceof Error) {
+            data.sqlConnection = error.message;
+        }
+        data.sqlConnection = 'error validating sql structure';
+    }
+
+    res.status(200).send(data);
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`⚡️[NODE_ENV]: ${NODE_ENV}`);
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 
@@ -25,4 +60,11 @@ app.listen(port, () => {
     logger.info(`======= ENV: ${NODE_ENV} =======`);
     logger.info(`🚀 [server]: Server is running at https://localhost:${port}`);
     logger.info(`=================================`);
+});
+
+process.on('SIGTERM', () => {
+    logger.info('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        logger.info('HTTP server closed');
+    });
 });
